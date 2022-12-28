9f1b048d6a17576abb33b3e3e7005344139201a3
lokanadham jyoshnavi <119840094+22a91a05i9@users.noreply.github.com>
2022-12-29 00:48:10 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         int N,M,c;         scanf("%d%d",&N,&M);         c=(5*N)+(7*M);         printf("%d ",c);     } }

---
+++ Tour.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        int N,M,c;
+        scanf("%d%d",&N,&M);
+        c=(5*N)+(7*M);
+        printf("%d
+",c);
+    }
+}
