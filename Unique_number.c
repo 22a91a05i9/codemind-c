f8b431bab9da68880e8441bdc28f77566b935549
lokanadham jyoshnavi <119840094+22a91a05i9@users.noreply.github.com>
2023-05-19 22:33:30 +0530

    #include<stdio.h> int main() {     int n,i,j,k,c=0,x[100],d;     scanf("%d",&n);     while(n!=0)     {         d=n%10;         n=n/10;         x[i]=d;         i++;     }     for(j=0;j<i;j++)     {         for(k=0;k<i;k++)         {             if(x[j]==x[k])             {                 c++;             }         }     }     if(c==i)     {         printf("Unique Number");     }     else     {         printf("Not Unique Number");     } }

---
+++ Unique_number.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,k,c=0,x[100],d;
+    scanf("%d",&n);
+    while(n!=0)
+    {
+        d=n%10;
+        n=n/10;
+        x[i]=d;
+        i++;
+    }
+    for(j=0;j<i;j++)
+    {
+        for(k=0;k<i;k++)
+        {
+            if(x[j]==x[k])
+            {
+                c++;
+            }
+        }
+    }
+    if(c==i)
+    {
+        printf("Unique Number");
+    }
+    else
+    {
+        printf("Not Unique Number");
+    }
+}
